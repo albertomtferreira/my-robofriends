9c256f8136b0f46b0fdd114d48cf76b7b66b064b
Alberto Ferreira <60177437+albertomtferreira@users.noreply.github.com>
2023-11-08 21:38:19 +0000

    update front end styles

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const SearchBox = ({searchfield, searchChange}) => {
   return[
-    <div className="pa2">
+    <div className="pa2 ">
       <input 
-        className="pa3 ba b--green bg-lightest-blue"
+        className="pa2 ba br3 br--top b--green bg-lightest-blue"
         type='search'
         placeholder='Search Robots'
         onChange={searchChange}
